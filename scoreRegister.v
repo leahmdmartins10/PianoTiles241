1a12d88d4eb8a976375caee8cd837e8f03415f1e
leahmdmartins10 <127546824+leahmdmartins10@users.noreply.github.com>
2023-12-04 23:21:59 -0500

    Update scoreRegister.v

--- scoreRegister.v
+++ scoreRegister.v
@@ -8,56 +8,56 @@ module score(clock, resetn, startn, current_state, increment, HEX0, HEX1, HEX2, 
 	
 	reg incrementdigits;
 	
-	reg[3:0] digit_0, digit_1, digit_2, digit_3, digit_4, digit_5;
+	reg[3:0] d0, d1, d2, d3, d4, d5;
 	output[6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5;
 	
 	// assigning each 4 bit digit to register Q in increments of 4, will display the digits
 	// onto hex display
 	
 	always @(*) begin
-		digit_0 = Q[3:0];
-		digit_1 = Q[7:4];
-		digit_2 = Q[11:8];
-		digit_3 = Q[15:12];
-		digit_4 = Q[19:16];
-		digit_5 = Q[23:20];
+		d0 = Q[3:0];
+		d1 = Q[7:4];
+		d2 = Q[11:8];
+		d3 = Q[15:12];
+		d4 = Q[19:16];
+		d5 = Q[23:20];
 		
-		if(digit_0 > 9) begin
-			digit_1 = digit_1 + 1; // carry over the one
-			digit_0 = digit_0 - 10; // remove the 10th carry over
+		if(d0 > 9) begin
+			d1 = d1 + 1; // carry over the one
+			d0 = d0 - 10; // remove the 10th carry over
 		end
-		if(digit_1 > 9) begin
-			digit_2 = digit_2 + 1; // carry over the one
-			digit_1 = digit_1 - 10; // remove the 10th carry over
+		if(d1 > 9) begin
+			d2 = d2 + 1; // carry over the one
+			d1 = d1 - 10; // remove the 10th carry over
 		end
-		if(digit_2 > 9) begin
-			digit_3 = digit_3 + 1; // carry over the one
-			digit_2 = digit_2 - 10; // remove the 10th carry over
+		if(d2 > 9) begin
+			d3 = d3 + 1; // carry over the one
+			d2 = d2 - 10; // remove the 10th carry over
 		end
-		if(digit_3 > 9) begin
-			digit_4 = digit_4 + 1; // carry over the one
-			digit_3 = digit_3 - 10; // remove the 10th carry over
+		if(d3 > 9) begin
+			d4 = d4 + 1; // carry over the one
+			d3 = d3 - 10; // remove the 10th carry over
 		end
-		if(digit_4 > 9) begin
-			digit_5 = digit_4 + 1; // carry over the one
-			digit_4 = digit_5 - 10; // remove the 10th carry over
+		if(d4 > 9) begin
+			d5 = d4 + 1; // carry over the one
+			d4 = d5 - 10; // remove the 10th carry over
 		end
-		if(digit_5 > 4'b1001) begin
-			digit_0 = 4'b1001;
-			digit_1 = 4'b1001;
-			digit_2 = 4'b1001;
-			digit_3 = 4'b1001;
-			digit_4 = 4'b1001;
-			digit_5 = 4'b1001;
+		if(d5 > 4'b1001) begin
+			d0 = 4'b1001;
+			d1 = 4'b1001;
+			d2 = 4'b1001;
+			d3 = 4'b1001;
+			d4 = 4'b1001;
+			d5 = 4'b1001;
 		end
 	end
 	
-	hexDecoder h0(.c3(digit_0[3]), .c2(digit_0[2]), .c1(digit_0[1]), .c0(digit_0[0]), .s(HEX0));
-	hexDecoder h1(.c3(digit_1[3]), .c2(digit_1[2]), .c1(digit_1[1]), .c0(digit_1[0]), .s(HEX1));
-	hexDecoder h2(.c3(digit_2[3]), .c2(digit_2[2]), .c1(digit_2[1]), .c0(digit_2[0]), .s(HEX2));
-	hexDecoder h3(.c3(digit_3[3]), .c2(digit_3[2]), .c1(digit_3[1]), .c0(digit_3[0]), .s(HEX3));
-	hexDecoder h4(.c3(digit_4[3]), .c2(digit_4[2]), .c1(digit_4[1]), .c0(digit_4[0]), .s(HEX4));
-	hexDecoder h5(.c3(digit_5[3]), .c2(digit_5[2]), .c1(digit_5[1]), .c0(digit_5[0]), .s(HEX5));
+	hexDecoder h0(.c3(d0[3]), .c2(d0[2]), .c1(d0[1]), .c0(d0[0]), .s(HEX0));
+	hexDecoder h1(.c3(d1[3]), .c2(d1[2]), .c1(d1[1]), .c0(d1[0]), .s(HEX1));
+	hexDecoder h2(.c3(d2[3]), .c2(d2[2]), .c1(d2[1]), .c0(d2[0]), .s(HEX2));
+	hexDecoder h3(.c3(d3[3]), .c2(d3[2]), .c1(d3[1]), .c0(d3[0]), .s(HEX3));
+	hexDecoder h4(.c3(d4[3]), .c2(d4[2]), .c1(d4[1]), .c0(d4[0]), .s(HEX4));
+	hexDecoder h5(.c3(d5[3]), .c2(d5[2]), .c1(d5[1]), .c0(d5[0]), .s(HEX5));
 	
 	always@(posedge clock) begin
 		if(!resetn | (!startn & current_state == 5'd0)) begin
